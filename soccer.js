057517b7fc880c8244e8e11a93f9e04be2a60214
msdkkprojeck <pasjackpot89@gmail.com>
2026-03-07 13:17:23 +0700

    Update soccer.js

--- soccer.js
+++ soccer.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded",function(){
 
 const ball=document.getElementById("ball")
 const keeper=document.getElementById("keeper")
+const player=document.getElementById("player")
 const result=document.getElementById("result")
 
 let startX,startY
@@ -23,6 +24,15 @@ kickBall(dx)
 
 function kickBall(dx){
 
+player.classList.add("kick")
+ball.classList.add("spin")
+
+setTimeout(()=>{
+
+player.classList.remove("kick")
+
+},200)
+
 ball.style.bottom="300px"
 
 let target=290+dx
@@ -34,13 +44,19 @@ ball.style.left=target+"px"
 
 let chance=Math.random()
 
-if(chance<0.2){
+if(chance<=0.2){
 
 result.innerHTML="GOAL ⚽"
 
 }else{
 
+keeper.classList.add("jump")
+
+setTimeout(()=>{
+
 result.innerHTML="SAVE 🧤"
+
+},300)
 
 }
 
@@ -50,8 +66,13 @@ setTimeout(resetBall,2000)
 
 function resetBall(){
 
+ball.classList.remove("spin")
+
 ball.style.bottom="80px"
 ball.style.left="290px"
+
+keeper.classList.remove("jump")
+
 result.innerHTML=""
 
 }
