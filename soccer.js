a0b802912c2b7acd14921daa17e1640f6d4bcf44
msdkkprojeck <pasjackpot89@gmail.com>
2026-03-07 13:09:10 +0700

    Update soccer.js

--- soccer.js
+++ soccer.js
@@ -1,48 +1,71 @@
-const ball = document.getElementById("ball")
-const keeper = document.getElementById("keeper")
-const result = document.getElementById("result")
+document.addEventListener("DOMContentLoaded",function(){
+
+const ball=document.getElementById("ball")
+const keeper=document.getElementById("keeper")
+const player=document.getElementById("player")
+const result=document.getElementById("result")
 
 let startX,startY
 
-document.addEventListener("mousedown",(e)=>{
+document.addEventListener("mousedown",function(e){
 
-startX = e.clientX
-startY = e.clientY
+startX=e.clientX
+startY=e.clientY
 
 })
 
-document.addEventListener("mouseup",(e)=>{
+document.addEventListener("mouseup",function(e){
 
-let dx = e.clientX - startX
-let dy = startY - e.clientY
+let dx=e.clientX-startX
+let dy=startY-e.clientY
 
 kickBall(dx,dy)
 
 })
 
+/* kiper bergerak otomatis */
+
+setInterval(function(){
+
+let pos=[220,260,300]
+
+keeper.style.left=pos[Math.floor(Math.random()*pos.length)]+"px"
+
+},1500)
+
 function kickBall(dx,dy){
+
+ball.classList.add("ballKick")
+
+player.classList.add("kick")
+
+setTimeout(()=>{
+
+player.classList.remove("kick")
+
+},200)
 
 ball.style.bottom="300px"
 
-let target = 280 + dx
+let target=290+dx
 
-if(target < 200) target = 200
-if(target > 360) target = 360
+if(target<200)target=200
+if(target>380)target=380
 
-ball.style.left = target+"px"
+ball.style.left=target+"px"
 
-let chance = Math.random()
+let chance=Math.random()
 
-if(chance <= 0.2){
+if(chance<=0.2){
 
 result.innerHTML="GOAL ⚽"
 
 }else{
 
-moveKeeper()
-
 setTimeout(()=>{
+
 result.innerHTML="SAVE 🧤"
+
 },300)
 
 }
@@ -51,19 +74,15 @@ setTimeout(resetBall,2000)
 
 }
 
-function moveKeeper(){
-
-let pos=[220,260,300]
-
-keeper.style.left=pos[Math.floor(Math.random()*pos.length)]+"px"
-
-}
-
 function resetBall(){
 
-ball.style.bottom="70px"
-ball.style.left="280px"
+ball.classList.remove("ballKick")
+
+ball.style.bottom="80px"
+ball.style.left="290px"
 
 result.innerHTML=""
 
 }
+
+})
